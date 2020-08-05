5a0b71d94a492405db1b17b6499f03d08002e7c7
RemahMassri96 <remah.massri@gmail.com>
2020-08-06 02:00:09 +0300

    router first approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lemon-mart';
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'lemon',
+      sanitizer.bypassSecurityTrustResourceUrl(
+      'assets/img/icons/lemon.svg'
+      )
+      );
+  }
 }
---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+
+const modules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+
+];
+
+@NgModule({
+  exports : modules
+})
+export class MaterialModule { }
